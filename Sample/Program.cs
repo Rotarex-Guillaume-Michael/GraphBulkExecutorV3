bedc0de300e05a4ff9c1618f8cbd869344df068d
Michael Guillaume <guillaume.michael@rotarex.com>
2022-07-25 10:24:36 +0200

    Add project

---
+++ Sample/Program.cs
@@ -0,0 +1,101 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace GraphBulkExecutorV3.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos;
+
+    class Program
+    {
+        private static string connectionString = "<******** CONNECTION STRING ********>";
+        private static string databaseName = "vConnect";
+        private static string containerName = "test";
+        private static long documentsToInsert = 1000;
+        private static string partitionKey = "PartitionKey";
+
+        static async Task Main(string[] args)
+        {
+            CosmosClient cosmosClient = new CosmosClient(connectionString);
+
+            Database database = await cosmosClient.CreateDatabaseIfNotExistsAsync(Program.databaseName);
+
+            await database.CreateContainerIfNotExistsAsync(Program.containerName, $"/{Program.partitionKey}", 10000);
+
+            GraphBulkExecutor graphBulkExecutor = new GraphBulkExecutor(connectionString, Program.databaseName, Program.containerName);
+
+            List<IGremlinElement> gremlinElements = new List<IGremlinElement>();
+            gremlinElements.AddRange(Program.GenerateVertices(Program.documentsToInsert));
+            gremlinElements.AddRange(Program.GenerateEdges(Program.documentsToInsert));
+            BulkOperationResponse bulkOperationResponse = await graphBulkExecutor.BulkImportAsync(
+                gremlinElements: gremlinElements,
+                enableUpsert: true);
+
+            Console.WriteLine($"TotalTimeTaken: {bulkOperationResponse.TotalTimeTaken}");
+            Console.WriteLine($"TotalRequestUnitsConsumed: {bulkOperationResponse.TotalRequestUnitsConsumed}");
+            Console.WriteLine($"SuccessfulDocuments: {bulkOperationResponse.SuccessfulDocuments}");
+        }
+
+        private static IEnumerable<GremlinEdge> GenerateEdges(long count)
+        {
+            
+            for (long i = 0; i < count - 1; i++)
+            {
+                string edgeId = Guid.NewGuid().ToString();
+
+                GremlinEdge e = new GremlinEdge(
+                    edgeId,
+                    "knows",
+                    "0",
+                    (i + 1).ToString(),
+                    "vertex",
+                    "vertex",
+                    Program.partitionKey,
+                    Program.partitionKey);
+
+                e.AddProperty("duration", i);
+
+                yield return e;
+            }
+
+            //for (long i = 0; i < count - 1; i++)
+            //{
+            //    GremlinEdge e = new GremlinEdge(
+            //        "e" + i,
+            //        "knows",
+            //        i.ToString(),
+            //        (i + 1).ToString(),
+            //        "vertex",
+            //        "vertex",
+            //        i,
+            //        i + 1);
+
+            //    e.AddProperty("duration", i);
+
+            //    yield return e;
+            //}
+        }
+
+        private static IEnumerable<GremlinVertex> GenerateVertices(long count)
+        {
+            GremlinVertex vBad = new GremlinVertex(Guid.NewGuid().ToString(), "vertex");
+            vBad.AddProperty(Program.partitionKey, 0);
+            yield return vBad;
+
+            for (long i = 0; i < count; i++)
+            {
+                GremlinVertex v = new GremlinVertex(i.ToString(), "vertex");
+                v.AddProperty(Program.partitionKey, Program.partitionKey);
+                v.AddProperty("name1", "name" + i);
+                v.AddProperty("name2", i * 2);
+                v.AddProperty("name3", i * 3);
+                v.AddProperty("name4", i + 100);
+
+                yield return v;
+            }
+        }
+    }
+}
